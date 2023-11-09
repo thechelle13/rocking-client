c7b2ea42b72a817392758300e71aa2e8006b1ba3
Michelle Totherow <tym4me13@gmail.com>
2023-11-09 06:28:27 -0600

    complete

--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -1,17 +1,49 @@
 import { useEffect } from "react"
 
-export const RockList = ({ rocks, fetchRocks }) => {
+export const RockList = ({ rocks, fetchRocks, showAll }) => {
     useEffect(() => {
-        fetchRocks()
-    }, [])
+        fetchRocks(showAll)
+    }, [showAll])
+
+    const handleDelete = async () => {
+        // deletion logic here
+
+         await fetch(`http://localhost:8000/rocks/${rock.id}`, {
+             method: "DELETE",
+            headers: {
+                 Authorization: `Token ${JSON.parse(localStorage.getItem("rock_token")).token}`
+             }
+         });
+              if (response.status === 204) {
+              fetchRocks(showAll)
+             }
+        
+    };
+
 
     const displayRocks = () => {
         if (rocks && rocks.length) {
-            return rocks.map(rock => <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
-                {rock.name} ({rock.type.label})
-                {/* <div>In the collection of {rock.user.first_name}{rock.user.last_name}</div> */}
-            </div>
-            )
+
+            return rocks.map(rock => (
+                <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
+                    {rock.name} ({rock.type.label})
+                    <div>In the collection of {rock.user && rock.user.first_name}{rock.user && rock.user.last_name}</div>
+                    <button onClick={async () => {
+                        const response = 
+                        await fetch(`http://localhost:8000/rocks/${rock.id}`, {
+                            method: "DELETE",
+                            headers: {
+                                Authorization: `Token ${JSON.parse(localStorage.getItem("rock_token")).token}`
+                            }
+                        });
+                             if (response.status === 204) {
+                             fetchRocks(showAll)
+                            }
+                        }}
+                        className="bg-red-500 text-white p-2 mt-2 rounded-md"
+                    >Delete</button>
+                </div>
+            ));
         }
 
         return <h3>Loading Rocks...</h3>
